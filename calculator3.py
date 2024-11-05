808b6619cb8eb931a9bccfbaf7abe32fb5bf2373
walkerataide <walker.ataide@ifal.edu.br>
2024-11-05 12:16:40 -0300

    add calculator2.py

---
+++ calculator3.py
@@ -0,0 +1,19 @@
+def main():
+    print("--- Calculadora Exponencial² 🤖 ---")
+    n = float(input("Qual o Número?"))
+    result = formatar(calcular_quadrado(n))
+    print(f"O quadrado de {formatar(n)} é {result}")
+    result = formatar(calcular_cubo(n))
+    print(f"O cubo de {formatar(n)} é {result}")
+
+def calcular_quadrado(n):
+    return n * n
+
+def calcular_cubo(n):
+    return n**3
+
+def formatar(n):
+    n = f"{n:_.2f}"
+    return n.replace(".", ",").replace("_", ".")
+
+main()
---
+++ hello2.py
@@ -0,0 +1,2 @@
+nome = input("Digite seu nome:")
+print(nome, 'é meu "amigo"', sep=" ", end="\n")
